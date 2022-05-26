0214febdf3a051835d2ea2e90193036b21662e34
Nandhakumar-nk <nandhakumar.sivakumar@ideas2it.com>
2022-05-26 21:34:12 +0530

    review changes-1(folder structure modified and demo1 deleted)

--- demo1/src/actions/toggleDisplay/toggleShedulingIcons.ts
+++
@@ -1,8 +0,0 @@
-import { ACTION_TYPES } from "../../constants/actionTypes";
-
-export function toggleShedulingIcons(displayShedulingIcons: boolean) {
-  return {
-    type: ACTION_TYPES.TOGGLE_SHEDULING_ICONS,
-    payload: displayShedulingIcons,
-  };
-}
--- demo1/src/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { fetchedDatumReducer } from './fetchedDatumReducer';
-import { toggleDisplayReducer } from './toggleDisplayReducer';
-
-export default combineReducers({
-  fetchedData: fetchedDatumReducer,
-  toggleDisplay: toggleDisplayReducer
-});

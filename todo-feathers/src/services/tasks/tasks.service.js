db0e294dd81eb7a25ef8602a92dc34a5ddfa545c
Nandhakumar-nk <nandhakumar.sivakumar@ideas2it.com>
2022-05-27 23:00:14 +0530

    review changes-11(custom service example added)

--- todo-feathers/src/services/tasks/tasks.service.js
+++ todo-feathers/src/services/tasks/tasks.service.js
@@ -1,19 +1,22 @@
 // Initializes the `tasks` service on path `/tasks`
-const { Tasks } = require('./tasks.class');
-const createModel = require('../../models/tasks.model');
-const hooks = require('./tasks.hooks');
+const { Tasks } = require("./tasks.class");
+const createModel = require("../../models/tasks.model");
+const hooks = require("./tasks.hooks");
 
-module.exports = function(app) {
-    const options = {
-        Model: createModel(app),
-        paginate: false,
-        whitelist: '$populate'
-    };
+module.exports = function (app) {
+  const options = {
+    Model: createModel(app),
+    paginate: false,
+    whitelist: "$populate",
+  };
 
-    // Initialize our service with any options it requires
-    app.use('/tasks', new Tasks(options, app));
-    // Get our initialized service so that we can register hooks
-    const service = app.service('tasks');
+  // Initialize our service with any options it requires
+  app.use("/tasks", new Tasks(options, app));
+  //custom service
+  //app.use('/importantTasks', {rest methods});
 
-    service.hooks(hooks);
+  // Get our initialized service so that we can register hooks
+  const service = app.service("tasks");
+
+  service.hooks(hooks);
 };
